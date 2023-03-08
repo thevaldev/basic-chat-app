9faeedf5a13063c8168273a93ab76cf8a448aba2
thevaldev <rajaniemivaltteri@gmail.com>
2023-03-09 00:37:37 +0200

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import "./style/app.scss";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <div className="wrap">
+          <header></header>
+          <div className="content">
+            
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
