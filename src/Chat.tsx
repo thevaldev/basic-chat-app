c178654e8ec861f2bd7e9d6313fc13aea81e05f0
thevaldev <rajaniemivaltteri@gmail.com>
2023-03-09 02:23:40 +0200

    Replaced svgs with fontawesome icons

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,11 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 function Chat() {
   return (
     <div className="chat">
       <div className="chat-message">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
@@ -22,9 +23,7 @@ function Chat() {
 
       <div className="chat-message">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
@@ -41,9 +40,7 @@ function Chat() {
 
       <div className="chat-message">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
@@ -60,9 +57,7 @@ function Chat() {
 
       <div className="chat-message">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
@@ -79,9 +74,7 @@ function Chat() {
 
       <div className="chat-message">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
@@ -98,9 +91,7 @@ function Chat() {
 
       <div className="chat-message">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
@@ -117,9 +108,7 @@ function Chat() {
 
       <div className="chat-message sender">
         <div className="avatar">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-          </svg>
+          <FontAwesomeIcon icon={faUser} />
         </div>
         <div className="message">
           <p>
