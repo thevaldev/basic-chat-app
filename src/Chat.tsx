b54040e2c4a20f2111c09d1d81d330bfa73c1943
thevaldev <rajaniemivaltteri@gmail.com>
2023-03-09 02:02:42 +0200

    Front-End Update

---
+++ src/Chat.tsx
@@ -0,0 +1,140 @@
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat-message">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            NAME <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="chat-message">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            NAME <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="chat-message">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            NAME <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="chat-message">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            NAME <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="chat-message">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            NAME <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="chat-message">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            NAME <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+
+      <div className="chat-message sender">
+        <div className="avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+          </svg>
+        </div>
+        <div className="message">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            maiores, provident harum sapiente explicabo minima, officiis cumque
+            voluptatibus sed ipsam rem quas modi culpa id magni molestiae illo
+            quam quaerat.
+          </p>
+          <span>
+            YOU <span className="date">09.03 00.48</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
