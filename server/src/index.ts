7bb2f4251088d6bcd2d2d8b31b8637ccbdf23d45
thevaldev <83673344+thevaldev@users.noreply.github.com>
2025-02-17 15:44:11 +0200

    update server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,24 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import * as http from 'http';
-import cors from 'cors';
-import { Server } from 'socket.io';
+import * as http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 
-io.on('connection', (socket) => {
-    console.log('user connected: ', socket.id);
-
-    socket.on('message', (data) => {
-        console.log(data.message + ' ' + socket.id);
-        
-        io.emit('receiveMessage', data);
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected: ', socket.id);
-    });
+io.on("connection", (socket) => {
+  io.emit("receiveMessage", { message: "New user connected" });
+  socket.on("message", (data) => {
+    io.emit("receiveMessage", data);
+  });
+  socket.on("disconnect", () => {
+    io.emit("receiveMessage", { message: "user disconnected" });
+  });
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
